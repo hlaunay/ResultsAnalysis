fd0fac876488b7c01f41032c80aae618d20758b3
hlaunay <herve.launay@gmail.com>
2014-08-25 23:33:27 +0200

    Configure appengine

--- src/main/java/fr/antoinelaunay/resultsanalysis/endpoint/AthleteEndPoint.java
+++ src/main/java/fr/antoinelaunay/resultsanalysis/endpoint/AthleteEndPoint.java
@@ -1,7 +1,6 @@
 package fr.antoinelaunay.resultsanalysis.endpoint;
 
 import com.google.api.server.spi.config.Api;
-import com.google.api.server.spi.config.ApiMethod;
 import com.google.api.server.spi.config.ApiNamespace;
 import fr.antoinelaunay.resultsanalysis.bean.Athlete;
 
@@ -11,6 +10,9 @@ import fr.antoinelaunay.resultsanalysis.bean.Athlete;
 
 @Api(name = "athleteapi",
         version = "v1",
+        scopes = {Constants.EMAIL_SCOPE},
+        clientIds = {Constants.WEB_CLIENT_ID, Constants.ANDROID_CLIENT_ID, Constants.IOS_CLIENT_ID},
+        audiences = {Constants.ANDROID_AUDIENCE},
         namespace = @ApiNamespace(ownerDomain = "antoinelaunay.fr",
                 ownerName = "antoinelaunay.fr",
                 packagePath=""))
--- src/main/java/fr/antoinelaunay/resultsanalysis/endpoint/Constants.java
+++ src/main/java/fr/antoinelaunay/resultsanalysis/endpoint/Constants.java
@@ -4,7 +4,7 @@ package fr.antoinelaunay.resultsanalysis.endpoint;
  * Contains the client IDs and scopes for allowed clients consuming your API.
  */
 public class Constants {
-  public static final String WEB_CLIENT_ID = "replace this with your web client ID";
+  public static final String WEB_CLIENT_ID = "711690117724-mk7fl814lqus6juvm3f2iiehn6rg6p5k.apps.googleusercontent.com";
   public static final String ANDROID_CLIENT_ID = "replace this with your Android client ID";
   public static final String IOS_CLIENT_ID = "replace this with your iOS client ID";
   public static final String ANDROID_AUDIENCE = WEB_CLIENT_ID;
